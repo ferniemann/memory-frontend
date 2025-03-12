fd16fea0e0b6708c33ccd92e3993e53265990e93
Ferdinand <hello@ferdi.digital>
2025-03-12 21:32:46 +0100

    modify header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,8 +1,10 @@
 <template>
   <header class="bg-violet-100 p-4 flex justify-between items-center">
     <h1 class="text-xl font-black text-violet-700">Memory Game</h1>
-    <button @click="$router.push('/')"
-      class="bg-violet-700 text-white rounded py-2 px-6 hover:bg-violet-900 cursor-pointer disabled:pointer-events-none disabled:opacity-50"
-      :disabled="$route.name === 'home'">Neues Spiel</button>
+    <BtnNewGame />
   </header>
 </template>
+
+<script setup>
+import BtnNewGame from "@/components/new-game/BtnNewGame.vue"
+</script>
