740e310b99f4552c54525991acc35a55cdf72b78
Ferdinand <hello@ferdi.digital>
2025-03-12 21:36:03 +0100

    finish app screen

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
 <template>
-  <RouterView />
+  <MainHeader />
+  <main class="px-4 py-8">
+    <RouterView />
+  </main>
 </template>
+
+<script setup>
+import MainHeader from "@/components/MainHeader.vue"
+</script>
