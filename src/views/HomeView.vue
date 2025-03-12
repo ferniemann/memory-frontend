a67e1eb93d64cab0bb3290f5df643e7088259b9f
Ferdinand <hello@ferdi.digital>
2025-03-12 21:33:19 +0100

    game options screen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <template>
-  <main>
-    <h1>Home</h1>
-  </main>
+  <NewGameOptions />
+  <RouterLink :to="{ name: 'game' }"
+    class="bg-violet-700 text-white rounded py-2 px-6 hover:bg-violet-900 cursor-pointer my-8 inline-block">
+    Spiel starten
+  </RouterLink>
 </template>
+
+<script setup>
+import NewGameOptions from "@/components/new-game/NewGameOptions.vue"
+</script>
