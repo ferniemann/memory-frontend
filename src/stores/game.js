b76ed08191e057948e481a000643bbdfd2f55bd6
Ferdinand <hello@ferdi.digital>
2025-03-12 19:01:00 +0100

    implement fetch for cards

--- src/stores/game.js
+++ src/stores/game.js
@@ -1,3 +1,13 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useGameStore = defineStore('game', () => {})
+export const useGameStore = defineStore('game', () => {
+  const cards = ref(null)
+
+  async function getAllCards() {
+    const response = await fetch(import.meta.env.VITE_API_URL)
+    cards.value = await response.json()
+  }
+
+  return { cards, getAllCards }
+})
---
+++ src/views/GameView.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Game</h1>
+</template>
+
+<script setup>
+
+import { onMounted } from "vue"
+import { useGameStore } from "@/stores/game.js"
+
+const store = useGameStore()
+
+onMounted(async () => {
+  await store.getAllCards()
+
+  console.log(store.cards);
+})
+</script>
